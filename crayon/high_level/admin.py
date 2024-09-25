7867b156591e67f0b3b346c6d9e0902392b1f6cd
Youssef AIT TALEB <aittaleb.youss@gmail.com>
2024-09-25 15:27:44 +0200

    Model Ville créé

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -1,1 +1,8 @@
 # Register your models here.
+
+
+# election/admin.py
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Ville)
--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+
 # Create your models here.
+class Ville(models.Model):
+    nom = models.CharField(max_length=100)
+    code_postale = models.IntegerField()
+    prix = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom}"
